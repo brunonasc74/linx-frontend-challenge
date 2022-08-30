d6b1c80925f275a6339bac6a1cd548d6730be627
brunonasc74 <bruno.hpn74@gmail.com>
2022-08-30 13:57:14 -0300

    Footer component created

---
+++ src/components/Footer.jsx
@@ -0,0 +1,11 @@
+const Footer = () => {
+	return (
+		<footer>
+			<p>Testando suas habilidades em HTML, CSS e JS.</p>
+			<p>Linx Impulse</p>
+			<p>2019</p>
+		</footer>
+	);
+};
+
+export default Footer;
