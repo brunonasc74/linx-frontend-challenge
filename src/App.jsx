c22274aef5ee4051b733d26c72dd2a00895faf18
brunonasc74 <bruno.hpn74@gmail.com>
2022-08-31 05:32:21 -0300

    code refactored

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,20 @@
 import { useState } from 'react';
 import Header from './components/Header';
-import MainSection from './components/MainSection';
+import Form from './components/Form';
+import Section from './components/Section';
+import Title from './components/Title';
 import Footer from './components/Footer';
 
 function App() {
 	return (
 		<div className='App'>
 			<Header />
-			<MainSection />
+			<main>
+				<Section />;
+				<Form />
+				<Title text={'Sua seleção especial'} />
+				<Title text={'Compartilhe a novidade'} />
+			</main>
 			<Footer />
 		</div>
 	);
