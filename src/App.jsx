98bbec26bc54580f2949f4506520f495fc21dc15
brunonasc74 <bruno.hpn74@gmail.com>
2022-08-31 04:02:02 -0300

    MainSection component created

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import { useState } from 'react';
 import Header from './components/Header';
+import MainSection from './components/MainSection';
 import Footer from './components/Footer';
 
 function App() {
 	return (
 		<div className='App'>
 			<Header />
+			<MainSection />
 			<Footer />
 		</div>
 	);
---
+++ src/components/MainSection.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Form from './Form';
+import Section from './Section';
+
+const MainSection = () => {
+	return (
+		<main>
+			<Section />;
+			<Form />
+		</main>
+	);
+};
+
+export default MainSection;
