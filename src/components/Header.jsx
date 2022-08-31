20211c90ef6eebfe93fe66db05c7fda2028d4810
brunonasc74 <bruno.hpn74@gmail.com>
2022-08-31 03:59:06 -0300

    Header refactored

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,25 +1,23 @@
 const Header = () => {
 	return (
 		<header>
-			<div className='outer-div'>
-				<div className='container'>
-					<section>
-						<div className='box-one'>
-							<h3>uma selecão de produtos</h3>
-							<h1>especial para você</h1>
-							<p>
-								Todos os produtos desta lista foram selecionados a partir da sua
-								navegação. Aproveite!
-							</p>
-						</div>
-					</section>
-				</div>
-				<div className='box-two'>
-					<div className='box'>Conheça a Linx</div>
-					<div className='box'>Ajude o algorítimo</div>
-					<div className='box'>Seus produtos</div>
-					<div className='box'>Compartilhe</div>
-				</div>
+			<div className='container'>
+				<section>
+					<div className='box-one'>
+						<h3>uma selecão de produtos</h3>
+						<h1>especial para você</h1>
+						<p>
+							Todos os produtos desta lista foram selecionados a partir da sua
+							navegação. Aproveite!
+						</p>
+					</div>
+				</section>
+			</div>
+			<div className='box-two'>
+				<div className='box'>Conheça a Linx</div>
+				<div className='box'>Ajude o algorítimo</div>
+				<div className='box'>Seus produtos</div>
+				<div className='box'>Compartilhe</div>
 			</div>
 		</header>
 	);
